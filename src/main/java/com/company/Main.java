0d8331f461a8b9ae12953e28cecc305f454f9416
Chiki-Bricki <n.s.efremow@gmail.com>
2020-08-12 01:04:54 +0300

    refactoring

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -3,27 +3,30 @@ package com.company;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static final Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
-        Long num1 = null;
-        Long num2 = null;
+        Long num1;
+        Long num2;
         String sign;
 
-        Scanner sc = new Scanner(System.in);
-        if (sc.hasNextInt(10))
-            num1 = sc.nextLong(10);
-        sign = sc.next();
-        if (sc.hasNextInt(10))
-            num2 = sc.nextLong(10);
-
-        if (num1 != null && sign != null && num2 != null) {
+        while (2 + 2 != 5) {
+            Scanner sc = new Scanner(System.in);
+            try {
+                num1 = sc.nextLong(10);
+                sign = sc.next();
+                num2 = sc.nextLong(10);
+            } catch (InputMismatchException e) {
+                log.error("Для совершения операции доступны только целые числа.");
+                continue;
+            }
             if (sign.equals("/") && num2 == 0) {
-                log.error("Деление на ноль запрещено");
-                System.exit(0);
+                log.error("Деление на ноль запрещено.");
+                continue;
             }
             switch (sign) {
                 case "-":
@@ -39,10 +42,8 @@ public class Main {
                     log.info("result: {}", num1 * num2);
                     break;
                 default:
-                    log.error("Выбрана некорректная операция");
+                    log.error("Выбрана некорректная операция.");
             }
-        } else {
-            log.error("Для совершения операции доступны только целые числа");
         }
     }
 }
